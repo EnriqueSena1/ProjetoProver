ac2c07c36ce9c6347fa66829c702fce04d367d30
Enrique Sena <enriquesenacontato07@gmail.com>
2025-04-24 21:31:19 -0400

    Feature - Criando as Classas
    Criando as Classes no Model, e finalizando as configurações.

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'website'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +77,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Bancoprover',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- website/models.py
+++ website/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    is_adm = models.BooleanField(default=False)
+    cpf = models.CharField(max_length=11, default=False)
+
+
+class Cliente(models.Model):
+    nome = models.models.CharField(max_length=50, null=False, blank=False)
+    cpf = models.CharField(max_length= 11, default=False)
+    tell = models.CharField(max_length= 11, null=False, default=False)
+
+class Produto(models.Model):
+    nome = models.models.CharField(max_length=50, null=False, blank=False)
+    qtd_prod = models.IntegerField(null=False, blank=False)
+    valor = models.IntegerField (null=False, blank=False)
+    tipo_choices = [("Perecivel","Perecivel"), ("Congelado","Congelado"), ("Não Pereciveis", "Não Pereciveis")]
+    tipo_prod = models.CharField(max_length=16, choices = tipo_choices)
+    descricao = models.TextField()
+    img_prod = models.models.CharField(max_length=150, null=False, default= False)
+
+
+    
